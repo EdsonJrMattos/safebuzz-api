6f4ac76017cc86d755cb396aa1a707ce4139fa79
EdsonJrMattos <edsonjuniortdeveloper@gmail.com>
2016-11-23 19:59:32 -0200

    create  controller and model River

---
+++ api/controllers/RiverController.js
@@ -0,0 +1,11 @@
+/**
+ * RiverController
+ *
+ * @description :: Server-side logic for managing rivers
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
