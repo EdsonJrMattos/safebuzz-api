acb5e3430676fde8da3e4a729e0b7926f627ed77
EdsonJrMattos <edsonjuniortdeveloper@gmail.com>
2016-11-23 19:59:18 -0200

    create  controller and model River Head

---
+++ api/controllers/RiverheadController.js
@@ -0,0 +1,11 @@
+/**
+ * RiverheadController
+ *
+ * @description :: Server-side logic for managing riverheads
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
