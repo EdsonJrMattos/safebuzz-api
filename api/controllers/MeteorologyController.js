dbbfa554f751509a46efdc5c7886c3000a1a2424
EdsonJrMattos <edsonjuniortdeveloper@gmail.com>
2016-11-23 20:00:30 -0200

    create  controller and model Meteorology

---
+++ api/controllers/MeteorologyController.js
@@ -0,0 +1,11 @@
+/**
+ * MeteorologyController
+ *
+ * @description :: Server-side logic for managing meteorologies
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
